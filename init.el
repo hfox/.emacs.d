64da972ad243e9f541fd3fbb8113ad5f1187a720
valentin <valentin@assmundsson.com>
2018-01-03 17:02:59 +0100

    add org-mode

--- init.el
+++ init.el
@@ -1,4 +1,11 @@
 ;; I like tabs
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (setq tab-width 4)
 (setq indent-tabs-mode t)
 
@@ -8,3 +15,18 @@
 	    (setq tab-width 4)
 	    (setq python-indent-offset 4)
 	    (setq indent-tabs-mode t)))
+
+
+;; org-mode, append time of closure to todos
+(setq org-log-done 'time)
+
+;; MELPA
+(require 'package) ;; You might already have this line
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.org/packages/"))
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize) ;; You might already have this line
+
+(load "~/.emacs.d/secret.el")
